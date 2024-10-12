c28c15b2a8083d70ee0a4d5bb0230d8ac027067a
yankas <christopher.sbrisny@gmail.com>
2024-10-12 22:57:08 +0200

    .

--- modules/home-manager/desktop/savegames.nix
+++ modules/home-manager/desktop/savegames.nix
@@ -1,11 +1,11 @@
 { config, pkgs, lib, ... }:
+let
+  link = config.lib.file.mkOutOfStoreSymlink;
+in
 {
-
-
-
   config = {
-    home.file {
-      ".local/share/NeposGames/Nebuchadnezzar" = config.lib.file.mkOutOfStoreSymlink "/home/${config.home.username}/.saves/Nebuchadnezzar"
+    home.file = {
+      ".local/share/NeposGames/Nebuchadnezzar" = link "/home/${config.home.username}/.saves/Nebuchadnezzar"
     };
   };
 }
