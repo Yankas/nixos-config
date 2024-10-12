9a3329fba360a86bee8887c2d46767e9bcaf9977
yankas <christopher.sbrisny@gmail.com>
2024-10-12 22:55:43 +0200

    .

--- modules/home-manager/desktop/savegames.nix
+++ modules/home-manager/desktop/savegames.nix
@@ -1,17 +1,11 @@
 { config, pkgs, lib, ... }:
 {
-  options = {
-    programs.nintendo.gba.enable = lib.mkEnableOption "enables nintendo ds emulator";
-    programs.nintendo.ds.enable = lib.mkEnableOption "enables nintendo ds emulator";
-    programs.nintendo.switch.enable = lib.mkEnableOption "enables nintendo switch emulator";
-    programs.nintendo.swith-utils.enable = lib.mkEnableOption "enables tools to flash switch";
-  };
+
 
 
   config = {
-    home.packages = with pkgs;
-    (if config.programs.nintendo.ds.enable then [ melonDS ] else [])
-    ++ (if config.programs.nintendo.switch.enable then [ ryujinx ] else [])
-    ++ (if config.programs.nintendo.gba.enable then [ mgba ] else []);
+    home.file {
+      ".local/share/NeposGames/Nebuchadnezzar" = config.lib.file.mkOutOfStoreSymlink "/home/${config.home.username}/.saves/
+    };
   };
 }
