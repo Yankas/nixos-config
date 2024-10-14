b6b39e0d4962e846aaa5f22a0da57cb6190a15d3
yankas <christopher.sbrisny@gmail.com>
2024-10-14 15:44:24 +0200

    .

--- modules/system/desktop/dev-tools.nix
+++ modules/system/desktop/dev-tools.nix
@@ -11,15 +11,15 @@
     programs.godot.enable = lib.mkEnableOption "enables godot 4 engine";
     programs.tiled.enable = lib.mkEnableOption "enables tiled map editor";
   };
-
-  environment.systemPackages = with pkgs;
-    (if config.programs.godot.enable then [ godot4-mono dotnet-sdk_8] else [])
-    ++ (if config.programs.tiled.enable then [ tiled ] else [])
-    ++ (if config.programs.rider.enable then [ 
-      jetbrains.rider 
-      dotnet-sdk_8 
-      dotnet-sdk_7 
-      dotnet-sdk_6 
-      ] else []);
-    
+  config {
+    environment.systemPackages = with pkgs;
+      (if config.programs.godot.enable then [ godot4-mono dotnet-sdk_8] else [])
+      ++ (if config.programs.tiled.enable then [ tiled ] else [])
+      ++ (if config.programs.rider.enable then [ 
+        jetbrains.rider 
+        dotnet-sdk_8 
+        dotnet-sdk_7 
+        dotnet-sdk_6 
+        ] else []);
+  }
 }
