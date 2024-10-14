1ede53ef529ad26b7b4aa4f70662e3c014bc3edf
yankas <christopher.sbrisny@gmail.com>
2024-10-14 17:34:01 +0200

    .

--- modules/home-manager/desktop/hyprland.nix
+++ modules/home-manager/desktop/hyprland.nix
@@ -7,11 +7,11 @@ in
 {
   options = {
     autostart.exec = lib.mkOption {
-       type = with lib.types; attrsOf string;
+       type = with lib.types; listOf string;
        description = "list of commands to run on start";
     };
     autostart.exec-once = lib.mkOption {
-       type = with lib.types; attrsOf string;
+       type = with lib.types; listOf string;
        description = "list of commands to run on start";
     };
 
