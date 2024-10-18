1ab59a25149ce826fbe1a81a22ca7adc818b29d4
yankas <christopher.sbrisny@gmail.com>
2024-10-18 22:00:51 +0200

    .

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,6 @@ pkgs: {
   # example = pkgs.callPackage ./example { };
   godot4-mono = pkgs.callPackage ./godot4-mono/default.nix { };
   fighting-spirit = pkgs.callPackage ./fonts/fighting-spirit.nix { };
+  die-in-a-fire = pkgs.callPackage ./fonts/die-in-a-fire.nix { };
   yankas-wallpapers = pkgs.callPackage ./yankas-wallpapers/default.nix {};
 }
