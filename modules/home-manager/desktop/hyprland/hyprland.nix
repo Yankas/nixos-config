b59d54a58181cf734c3651b030e8b3abb71efc7c
yankas <christopher.sbrisny@gmail.com>
2024-10-21 00:50:58 +0200

    .

--- modules/home-manager/desktop/hyprland/hyprland.nix
+++ modules/home-manager/desktop/hyprland/hyprland.nix
@@ -53,8 +53,8 @@ in
         gaps_in = 5;
         gaps_out = 5;
         border_size = 2;
-        "col.active_border" = "rgba(${color.text-highlight}EE) rgba(${color.background-accent}EE) 45deg";
-        "col.inactive_border" = "rgba(${color.text}EE)";
+        "col.active_border" = "rgba(${color.text-highlight}AA) rgba(${color.background-accent}AA) 45deg";
+        "col.inactive_border" = "rgba(${color.text}AA)";
         # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
         allow_tearing = false;
         resize_on_border = true;
