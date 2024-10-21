435dd5efbf35d8ee75bc84fecfef8a313b346719
yankas <christopher.sbrisny@gmail.com>
2024-10-21 22:52:09 +0200

    .

--- modules/system/desktop/dev-tools.nix
+++ modules/system/desktop/dev-tools.nix
@@ -13,7 +13,7 @@
   };
   config = {
     environment.systemPackages = with pkgs;
-      (if config.programs.godot.enable then [ /*godot_4-mono*/ dotnet-sdk_8] else [])
+      (if config.programs.godot.enable then [ godot_4-mono dotnet-sdk_8] else [])
       ++ (if config.programs.tiled.enable then [ tiled ] else [])
       ++ (if config.programs.rider.enable then [ 
         jetbrains.rider 
