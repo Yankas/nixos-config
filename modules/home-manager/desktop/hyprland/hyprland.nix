51446b5f6e1250e244bcb970fb36685dc0c81973
yankas <christopher.sbrisny@gmail.com>
2024-10-23 19:02:11 +0200

    .

--- modules/home-manager/desktop/hyprland/hyprland.nix
+++ modules/home-manager/desktop/hyprland/hyprland.nix
@@ -345,7 +345,8 @@ in
         "workspace special:qbittorrent silent, initialClass:(^org.qbittorrent.qBittorrent$)"
       ] else [])
       ++ (if config.games.nintendo.switch.enable then [ 
-        "workspace name:$ws_games, initialClass:(^Ryujinx$)"
+        "workspace name:$ws_games silent, initialClass:(^Ryujinx$)"
+        "monitor $m_right, initialClass:(^Ryujinx$)"
       ] else []);      
     };
   };
