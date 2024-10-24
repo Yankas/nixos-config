7e52ac178c3d336c2f04c8cd71fe59e17c105ef4
yankas <christopher.sbrisny@gmail.com>
2024-10-24 03:06:42 +0200

    .

--- modules/home-manager/desktop/pcmanfm/default.nix
+++ modules/home-manager/desktop/pcmanfm/default.nix
@@ -7,20 +7,18 @@
 
 
   config.home.file = {
-    ".local/share/file-manager/actions/edit.desktop".text = 
-    ''
-      [Desktop Entry]
-      Type=Action
-      Profiles=profile_id
-      Name=Action name in English
-      Name[cc]=Action name in Language
-      Icon=Icon name
-      # Example: Icon=text-editor
-      
-      [X-Action-Profile profile_id]
-      MimeTypes=MIME-types (semicolon separated)
-      Exec=command and arguments
-      # Example: Exec=gedit %f
+    ".local/share/file-manager/actions/edit.desktop".text =  ''
+        [Desktop Entry]
+        Type=Action
+        Profiles=profile_id
+        Name=EDIT
+        Icon=Icon name
+        # Example: Icon=text-editor
+        
+        [X-Action-Profile profile_id]
+        MimeTypes=all/allfiles
+        Exec=exec gedit %f
+        # Example: Exec=gedit %f
     '';  
 
     ".local/share/pcmanfm/actions/edit.desktop".text = 
