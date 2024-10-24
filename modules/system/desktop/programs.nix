63a8f9b480e14f2eab19ddffbdc3f570ac1641a2
yankas <christopher.sbrisny@gmail.com>
2024-10-24 21:21:11 +0200

    .

--- modules/system/desktop/programs.nix
+++ modules/system/desktop/programs.nix
@@ -30,6 +30,19 @@
         cmake
       ] else []);
 
+
+      mkShell {
+        name = "dotnet-env";
+        packages = [
+          (with dotnetCorePackages; combinePackages [
+            sdk_6_0
+            sdk_7_0
+            sdk_8_0
+          ])
+          powershell
+        ];
+      };
+
     virtualisation.libvirtd = lib.mkIf config.programs.virtualization.enable  {
       enable = true;
     };
