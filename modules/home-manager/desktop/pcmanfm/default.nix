964d29732105ccedfa380fc38ed2ec21ec2601e9
yankas <christopher.sbrisny@gmail.com>
2024-10-24 03:07:35 +0200

    .

--- modules/home-manager/desktop/pcmanfm/default.nix
+++ modules/home-manager/desktop/pcmanfm/default.nix
@@ -23,18 +23,17 @@
 
     ".local/share/pcmanfm/actions/edit.desktop".text = 
     ''
-      [Desktop Entry]
-      Type=Action
-      Profiles=profile_id
-      Name=Action name in English
-      Name[cc]=Action name in Language
-      Icon=Icon name
-      # Example: Icon=text-editor
-      
-      [X-Action-Profile profile_id]
-      MimeTypes=MIME-types (semicolon separated)
-      Exec=command and arguments
-      # Example: Exec=gedit %f
+        [Desktop Entry]
+        Type=Action
+        Profiles=profile_id
+        Name=EDIT
+        Icon=Icon name
+        # Example: Icon=text-editor
+        
+        [X-Action-Profile profile_id]
+        MimeTypes=all/allfiles
+        Exec=exec gedit %f
+        # Example: Exec=gedit %f
     '';  
   };
 
