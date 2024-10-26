a2438825bf6050b47a3402cae6e30d7d408250c0
yankas <christopher.sbrisny@gmail.com>
2024-10-26 02:53:28 +0200

    .

--- dotnet.nix
+++ dotnet.nix
@@ -2,12 +2,25 @@ with import <nixpkgs> {};
 
 mkShell {
   name = "dotnet-env";
+  buildInputs = with pkgs; [
+    fontconfig
+    xorg.libX11
+    xorg.libX11.dev
+    zlib
+    libICE
+  ];
+  LD_LIBRARY_PATH = lib.makeLibraryPath [
+    fontconfig
+    xorg.libX11
+    xorg.libX11.dev
+    libICE
+    zlib
+  ];
   packages = [
     (with dotnetCorePackages; combinePackages [
       sdk_6_0
       sdk_7_0
       sdk_8_0
-      buildDotnetModule
     ])
   ];
 }
