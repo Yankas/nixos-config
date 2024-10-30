109f64a9ca5526f3ac33334a0b18a1a068a817cd
yankas <christopher.sbrisny@gmail.com>
2024-10-31 00:57:35 +0100

    .

--- flake.nix
+++ flake.nix
@@ -70,6 +70,7 @@
           # > Our main nixos configuration file <
           ./hosts/desktop/configuration.nix
           inputs.chaotic.nixosModules.default
+          style
         ];
       };
       server = nixpkgs.lib.nixosSystem {
