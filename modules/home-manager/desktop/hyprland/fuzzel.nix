8fc2c919a24508c0f8be0354c93776667f8b46e2
yankas <christopher.sbrisny@gmail.com>
2024-10-30 23:16:40 +0100

    .

--- modules/home-manager/desktop/hyprland/fuzzel.nix
+++ modules/home-manager/desktop/hyprland/fuzzel.nix
@@ -1,5 +1,11 @@
 { config, pkgs, lib, ... }:
 let yes = "yes"; in
+let colors = {
+  background        = "292B2E";
+  background-accent = "1A1A1A";
+  text              = "FDF6E3";
+  text-highlight    = "FF4221";
+}; in
 {
 
   xdg.configFile."fuzzel/fuzzel.ini".text = lib.generators.toINIWithGlobalSection { } {
@@ -10,13 +16,29 @@ let yes = "yes"; in
       icons-enabled = yes;
       #password-character = "*";
       lines = 20;
-      width = 50; # in characters
+      # width is in characters
+      width = 50;
+      # Number of spaces a tab is expanded to 
+      tabs = 4;
+      exit-on-keyboard-focus-loss = yes;
 
 
 
     };
 
     sections = {
+      colors = {
+        # BACKGROUND COLORS
+        background = "${colors.background}";
+        selection = "${colors.background}";
+        # TEXT COLORS
+        text = "${colors.text}";
+        input = "${colors.text-highlight}";
+        match = "${colors.text-highlight}";
+        selection-text = "${colors.text-highlight}";
+        selection-match = "${colors.text}";
+        counter = ${colors.text-highlight};
+      };
 
       border = {
         #width = 1;
