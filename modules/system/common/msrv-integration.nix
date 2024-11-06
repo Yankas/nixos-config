0dc609967b8f62da509da8e8897f31b7bc896887
yankas <christopher.sbrisny@gmail.com>
2024-11-06 22:54:47 +0100

    .

--- modules/system/common/msrv-integration.nix
+++ modules/system/common/msrv-integration.nix
@@ -1,11 +1,15 @@
-{ 
+{
   lib,
   config,
   pkgs,
-  ... 
-}: 
+  ...
+}:
 {
   options = {
+    msrv.host = {
+      type = with lib.types; str;
+      default = "192.168.1.2";
+    };
     msrv.mount.documents = lib.mkEnableOption "mounts msrv:/srv/documents to /doc";
     msrv.mount.video = lib.mkEnableOption "mounts msrv:/srv/video to /vid";
     msrv.mount.iso = lib.mkEnableOption "mounts msrv:/srv/iso to /iso";
@@ -13,33 +17,33 @@
     msrv.mount.pictures = lib.mkEnableOption "mounts msrv:/srv/pic to /pic";
   };
 
-  config = {
+  config = with config; {
     fileSystems."/doc" = {
-      device = "192.168.1.2:/srv/docs";
+      device = "${msrv.host}:/srv/docs";
       fsType = "nfs";
       options = [ "x-systemd.automount" "noauto" ];
     };
 
     fileSystems."/vid" = {
-      device = "192.168.1.2:/srv/video";
+      device = "${msrv.host}:/srv/video";
       fsType = "nfs";
       options = [ "x-systemd.automount" "noauto" ];
     };
 
     fileSystems."/iso" = {
-      device = "192.168.1.2:/srv/iso";
+      device = "${msrv.host}:/srv/iso";
       fsType = "nfs";
       options = [ "x-systemd.automount" "noauto" ];
     };
 
     fileSystems."/mus" = {
-      device = "192.168.1.2:/srv/music";
+      device = "${msrv.host}:/srv/music";
       fsType = "nfs";
       options = [ "x-systemd.automount" "noauto" ];
     };
 
     fileSystems."/pic" = {
-      device = "192.168.1.2:/srv/photo";
+      device = "${msrv.host}:/srv/photo";
       fsType = "nfs";
       options = [ "x-systemd.automount" "noauto" ];
     };
