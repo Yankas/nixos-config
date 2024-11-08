2610a0eff6a5575ac9fd58e1dbbcf0b17e3105d1
yankas <christopher.sbrisny@gmail.com>
2024-11-08 23:11:33 +0100

    .

--- pkgs/d2r-tools/default.nix
+++ pkgs/d2r-tools/default.nix
@@ -1,16 +1,17 @@
 { lib, fetchzip, fetchurl, pkgs }:
 fetchzip {
   name = "d2r-tools";
-  hash = "sha256-ofi5lJfJGL4/R/gYD0SQxczU3PCFFE5L9Nec1E4KXuE=";
+  hash = "sha256-qn94K26BFfLuZRa4dvf3Sxj3sPtT+Cpl6nbrQqkfXrg=";
   url = "http://nix.yank.as/d2r-tools.tar.gz";
 
   nativeBuildInputs = [];
   #stripRoot = false;
   downloadToTemp = true;
-  stripRoot = true;
+  stripRoot = false;
   postFetch = ''
     mkdir -p $out/share/d2rtools
-    mv build/*.exe $out/share/d2rtools/
+    ls
+    #mv build/*.exe $out/share/d2rtools/
     #tar -xzvf /build/d2r-tools.tar.gz  -C $out/share/d2rtools
   '';
 
