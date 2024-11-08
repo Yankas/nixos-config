ed06fc89f623c4a26bbbd1591aa30f534849be4c
yankas <christopher.sbrisny@gmail.com>
2024-11-08 23:05:29 +0100

    .

--- pkgs/d2r-tools/default.nix
+++ pkgs/d2r-tools/default.nix
@@ -1,8 +1,8 @@
 { lib, fetchzip, fetchurl, pkgs }:
 fetchzip {
   name = "d2r-tools";
-  #hash = "sha256-tcNyIXPXq68/+Hv8HsKx5z64xnY6zn8pbO6YQ3biUUI=";
-  url = "http://nix.yank.as/${name}.tar.gz";
+  hash = "sha256-ofi5lJfJGL4/R/gYD0SQxczU3PCFFE5L9Nec1E4KXuE=";
+  url = "http://nix.yank.as/d2r-tools.tar.gz";
 
   nativeBuildInputs = [];
   #stripRoot = false;
@@ -10,13 +10,13 @@ fetchzip {
   stripRoot = false;
   postFetch = ''
     mkdir -p $out/share/d2rtools
-    tar -xzvf -j /build/${name}.tar.gz  -d $out/share/d2rtools
+    tar -xzvf /build/d2r-tools.tar.gz  -C $out/share/d2rtools
   '';
 
   meta = with lib; {
     homepage = "https://comicfontsby.tehandeh.com/";
     description = "Tools for modding Diablo II Ressurected, including CascViewer and D2EXcel";
-    license = lib.licenses.unfree;
+    #license = lib.licenses.unfree;
     platforms = platforms.all;
   };
 }
