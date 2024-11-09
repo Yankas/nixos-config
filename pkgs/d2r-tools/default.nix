668e2fc5fbf76db708ebadaec1cf2b33f1fab442
yankas <christopher.sbrisny@gmail.com>
2024-11-10 00:52:58 +0100

    .

--- pkgs/d2r-tools/default.nix
+++ pkgs/d2r-tools/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, which, lib, fetchzip, fetchurl, pkgs }:
+{ stdenv, lib, fetchzip, fetchurl, pkgs, makeDesktopItem }:
 let
   d2r-tools-exe = fetchzip {
     name = "d2r-tools";
@@ -14,29 +14,42 @@ let
     '';
 
     meta = with lib; {
-      homepage = "https://comicfontsby.tehandeh.com/";
       description = "Tools for modding Diablo II Ressurected, including CascViewer and D2EXcel";
       #license = lib.licenses.unfree;
       platforms = platforms.all;
     };
   };
 
+  desktop = makeDesktopItem {
+    name = "cascviewer";
+    desktopName = "CascViewer";
+    comment = "Tool to view Casc Files for various Blizzard games";
+    genericName = "Archive Viewer";
+    categories = [ "Game" ];
+    exec = "cascviewer";
+  };
+
+
   d2r-tools = stdenv.mkDerivation {
       name = "d2r-tools";
         buildInputs = [
           d2r-tools-exe
-          which
         ];
 
         unpackPhase = "true";
 
         installPhase = ''
           mkdir -p $out/bin
+          mkdir -p $out/share/applications
           printf "#!/bin/sh\nwine ${d2r-tools-exe}/CascViewer.exe" >> $out/bin/cascviewer
           printf "#!/bin/sh\nwine ${d2r-tools-exe}/D2ExcelPlus.exe" >> $out/bin/d2excel
           printf "#!/bin/sh\nwine ${d2r-tools-exe}/d2rlint.exe" >> $out/bin/d2rlint
           chmod 0755 $out/bin/*
+
+          ln -s ${desktop}/share/applications/* $out/share/applications
         '';
-  };
-  in
+      description = "Tools for modding Diablo II Ressurected, including CascViewer and D2EXcel";
+      #license = lib.licenses.unfree;
+      #platforms = platforms.all;
+  }; in
 d2r-tools
