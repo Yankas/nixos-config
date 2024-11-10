d548059d1123331aa6054c6fbf7d5e8ec5265b01
yankas <christopher.sbrisny@gmail.com>
2024-11-10 01:03:41 +0100

    .

--- pkgs/d2r-tools/default.nix
+++ pkgs/d2r-tools/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchzip, fetchurl, pkgs, makeDesktopItem }:
+{ stdenv, lib, fetchzip, fetchurl, pkgs, makeDesktopItem, copyDesktopItems }:
 let
   d2r-tools-exe = fetchzip {
     name = "d2r-tools";
@@ -20,33 +20,49 @@ let
     };
   };
 
-  desktop = makeDesktopItem {
-    name = "cascviewer";
-    desktopName = "CascViewer";
-    comment = "Tool to view Casc Files for various Blizzard games";
-    genericName = "Archive Viewer";
-    categories = [ "Game" ];
-    exec = "cascviewer";
-  };
 
 
-  d2r-tools = stdenv.mkDerivation {
+
+  d2r-tools = stdenv.mkDerivation rec {
+      desktopItems = [
+        (makeDesktopItem {
+          name = "cascviewer";
+          desktopName = "CascViewer";
+          comment = "Tool to view Casc Files for various Blizzard games";
+          genericName = "Archive Viewer";
+          categories = [ "Game" ];
+          exec = "cascviewer";
+        })
+        (makeDesktopItem {
+          name = "d2excel";
+          desktopName = "D2Excel";
+          comment = "Tool to edit Diablo II spreadsheets";
+          genericName = "Spreadsheet Editor";
+          categories = [ "Game" ];
+          exec = "cascviewer";
+        })
+      ];
+
+
+
       name = "d2r-tools";
+        nativeBuildInputs = [
+          copyDesktopItems
+        ];
         buildInputs = [
           d2r-tools-exe
+
         ];
 
         unpackPhase = "true";
 
         installPhase = ''
           mkdir -p $out/bin
-          mkdir -p $out/share/applications
           printf "#!/bin/sh\nwine ${d2r-tools-exe}/CascViewer.exe" >> $out/bin/cascviewer
           printf "#!/bin/sh\nwine ${d2r-tools-exe}/D2ExcelPlus.exe" >> $out/bin/d2excel
           printf "#!/bin/sh\nwine ${d2r-tools-exe}/d2rlint.exe" >> $out/bin/d2rlint
           chmod 0755 $out/bin/*
 
-          ln -s ${desktop}/share/applications/* $out/share/applications
         '';
       description = "Tools for modding Diablo II Ressurected, including CascViewer and D2EXcel";
       #license = lib.licenses.unfree;
