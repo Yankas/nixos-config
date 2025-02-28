43408a8144b68b355a22fedf32a904d0017eaa8c
yankas <christopher.sbrisny@gmail.com>
2025-02-28 01:06:21 +0100

    .

--- modules/home-manager/desktop/hyprland/hyprland.nix
+++ modules/home-manager/desktop/hyprland/hyprland.nix
@@ -178,7 +178,7 @@ in
         direct_scanout = false;
       };
 
-      animations = mkIf config.laptopMode.enable == false {
+      animations = mkIf (config.laptopMode.enable == false) {
         enabled = true;
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
         animation =
