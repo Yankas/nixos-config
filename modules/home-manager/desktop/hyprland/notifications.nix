f98cd777ef8f899c479f1d212065fa18d2d8399e
yankas <christopher.sbrisny@gmail.com>
2025-03-03 01:21:09 +0100

    .

--- modules/home-manager/desktop/hyprland/notifications.nix
+++ modules/home-manager/desktop/hyprland/notifications.nix
@@ -4,6 +4,14 @@
     enable = true;
     settings = {
       global = {
+        font = "Droid Sans 10";
+        format = "<b>%s</b>\\n%b";
+        frame_color = "#555555";
+        frame_width = 2;
+        geometry = "500x5-5+30";
+        horizontal_padding = 8;
+        icon_position = "off";
+        line_height = 0;
         markup = "full";
         padding = 8;
         separator_color = "frame";
