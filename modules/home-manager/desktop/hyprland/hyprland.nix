7ec0f046e0cac6dbd2d8ce54b244bc1fa7ff3cda
yankas <christopher.sbrisny@gmail.com>
2025-08-04 00:47:01 +0200

    .

--- modules/home-manager/desktop/hyprland/hyprland.nix
+++ modules/home-manager/desktop/hyprland/hyprland.nix
@@ -272,41 +272,20 @@ in
         "$mod, up, movefocus, u"
         "$mod, down, movefocus, d"
 
-        # Switch workspaces with mainMod + [0-9]
 
-      ] ++
-      lib.lists.forEach [ 1 2 3 4 5 6 7 8 9 10] (wsNum:
+
+      ] ++  # Switch workspaces with mainMod + [0-9]
+      lib.lists.forEach [ 1 2 3 4 5 6 7 8 9 10 "$wsgames"] (wsNum:
         let ws = toString wsNum; in
         "$mod, ${ws}, workspace, ${ws}"
       )
-      ++
-      lib.lists.forEach [ 1 2 3 4 5 6 7 8 9 10] (wsNum:
+      ++  # Move active window to a workspace with mainMod + SHIFT + [0-9]
+      lib.lists.forEach [ 1 2 3 4 5 6 7 8 9 10 "$wsgames"] (wsNum:
         let ws = toString wsNum; in
         "$mod SHIFT, ${ws}, movetoworkspace, ${ws}"
       ) ++
       [
-        "$mod, 1, workspace, 1"
-        "$mod, 2, workspace, 2"
-        "$mod, 3, workspace, 3"
-        "$mod, 4, workspace, 4"
-        "$mod, 5, workspace, 5"
-        "$mod, 6, workspace, 6"
-        "$mod, 7, workspace, 7"
-        "$mod, 8, workspace, 8"
-        "$mod, 9, workspace, 9"
-        "$mod, 0, workspace, 10"
         "$mod, G, workspace, name:$ws_games"
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mod SHIFT, 1, movetoworkspace, 1"
-        "$mod SHIFT, 2, movetoworkspace, 2"
-        "$mod SHIFT, 3, movetoworkspace, 3"
-        "$mod SHIFT, 4, movetoworkspace, 4"
-        "$mod SHIFT, 5, movetoworkspace, 5"
-        "$mod SHIFT, 6, movetoworkspace, 6"
-        "$mod SHIFT, 7, movetoworkspace, 7"
-        "$mod SHIFT, 8, movetoworkspace, 8"
-        "$mod SHIFT, 9, movetoworkspace, 9"
-        "$mod SHIFT, 0, movetoworkspace, 10"
         "$mod SHIFT, G, movetoworkspace, name:$ws_games"
 
         # Scroll through existing workspaces with mainMod + scroll
