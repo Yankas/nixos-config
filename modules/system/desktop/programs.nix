2eecc0c4ad67fcb5927964be31e1a3f9f46eb168
yankas <christopher.sbrisny@gmail.com>
2025-09-19 20:38:26 +0200

    .

--- modules/system/desktop/programs.nix
+++ modules/system/desktop/programs.nix
@@ -1,13 +1,13 @@
-{ 
+{
   lib,
   config,
   pkgs,
-  ... 
-}: 
+  ...
+}:
 {
   options = {
     programs.rider.enable = lib.mkEnableOption "enables dotnet development enviornment";
-    programs.sddm-dev.enable = lib.mkEnableOption "enables tools for sddm theme creation";  
+    programs.sddm-dev.enable = lib.mkEnableOption "enables tools for sddm theme creation";
     programs.godot.enable = lib.mkEnableOption "enables godot 4 engine";
     programs.tiled.enable = lib.mkEnableOption "enables tiled map editor";
     programs.virtualization.enable = lib.mkEnableOption "enables tiled map editor";
@@ -18,11 +18,13 @@
     environment.systemPackages = with pkgs;
       (if config.programs.godot.enable then [ godot4-mono dotnet-sdk_8] else [])
       ++ (if config.programs.tiled.enable then [ tiled ] else [])
-      ++ (if config.programs.rider.enable then [ 
-        jetbrains.rider 
-        dotnet-sdk_8 
-        dotnet-sdk_7 
-        dotnet-sdk_6
+      ++ (if config.programs.rider.enable then [
+        jetbrains.rider
+
+        dotnetCorePackages.sdk_9_0
+        dotnetCorePackages.sdk_8_0
+        dotnetCorePackages.sdk_7_0
+        dotnetCorePackages.sdk_6_0
         fontconfig
         ] else [])
       ++ (if config.programs.sddm-dev.enable then [
@@ -30,7 +32,7 @@
         qtcreator
         cmake
       ] else []);
-      
+
       environment.variables = {
         LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath ([
           ]
@@ -43,5 +45,3 @@
     programs.virt-manager.enable = lib.mkIf config.programs.virtualization.enable true;
   };
 }
-
-
